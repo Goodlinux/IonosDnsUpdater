daef1b9d5ee751dac222a1c6ebc9fc60674af572
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2022-08-08 21:57:27 +0200

    Update updateDns.sh

--- updateDns.sh
+++ updateDns.sh
@@ -23,14 +23,14 @@ kill_script=false
 ###################################
 
 function Help() {
-	# Show Help
-	echo "If you need further help than the below, read the readme file \n
-	or create an issue on github."
+     # Show Help
+     echo "If you need further help than the below, read the readme file \n
+            or create an issue on github."
     echo "Syntax update.sh [-a|-e|-f|-v]."
     echo "options:"
     echo "-a	change dns entry to given ip adress"
-	echo "-e	show error codes"
-	echo "-f	redirect verbose output to file"
+    echo "-e	show error codes"
+    echo "-f	redirect verbose output to file"
     echo "-v	give verbose output"
     echo
 }
@@ -57,81 +57,58 @@ function RetrieveIpAdress() {
 }
 
 function RetrieveZoneId() {
-
-	log "Retrieving zone id."
-
-	# get zone ID
-	zone_id=$(curl -X GET "$base_url$dns_zone" -H "$curl_param $api_key" -s );
-	
-	# check if valid object was found
-	name=$(echo $zone_id | jq '.[] | .name?' );
-	
-	if [[ "$name" == "" ]]
+    log "Retrieving zone id."
+    # get zone ID
+    zone_id=$(curl -X GET "$base_url$dns_zone" -H "$curl_param $api_key" -s );
+    # check if valid object was found
+    name=$(echo $zone_id | jq '.[] | .name?' );
+    if [[ "$name" == "" ]]
 	then 
-		# exit with error 
-		echo "Error: $zone_id | jq '.[]'"
-		exit 2
-	fi
-
-	zone_id=$(echo $zone_id | jq '.[] | .id?' | tr -d '"');
-	log "Zoneid was set to $zone_id."
+	# exit with error 
+	echo "Error: $zone_id | jq '.[]'"
+	exit 2
+    fi
+    zone_id=$(echo $zone_id | jq '.[] | .id?' | tr -d '"');
+    log "Zoneid was set to $zone_id."
 }
 
-function DeleteRecord() {
-	
-	log "Deleting record $1."
-
-	delete_url="$base_url$dns_zone/$zone_id/records/$1"
-
-	curl -X DELETE $delete_url -H "accept: */*" -H "$curl_param $api_key"
+function DeleteRecord() {	
+    log "Deleting record $1."
+    delete_url="$base_url$dns_zone/$zone_id/records/$1"
+    curl -X DELETE $delete_url -H "accept: */*" -H "$curl_param $api_key"
 }
 
 function GetCustomerZone() {
-
-	log "Retrieving dns records."
-
-	customer_url="$base_url$dns_zone/$zone_id?recordType=$dns_type"
-	
-	records=$(curl -X GET $customer_url -H $output_type -H "$curl_param $api_key" -s | jq '.records')
-	
-	log "Find maching domain record."
-
-	echo $records | jq -c '.[]'  | while read i; do
-
-		name=$(echo $i | jq '.name' | tr -d '"')
-		
-		if [[ $name = "$domain" || $name = "www.$domain" ]];
+    log "Retrieving dns records."
+    customer_url="$base_url$dns_zone/$zone_id?recordType=$dns_type"	
+    records=$(curl -X GET $customer_url -H $output_type -H "$curl_param $api_key" -s | jq '.records')
+    log "Find maching domain record."
+    echo $records | jq -c '.[]'  | while read i; do
+    name=$(echo $i | jq '.name' | tr -d '"')
+    if [[ $name = "$domain" || $name = "www.$domain" ]];
+	then
+	    log "Matching record found."
+	    current_ip=$(echo $i | jq '.content' | tr -d '"')
+	   if [[ "$current_ip" == "$ip" ]];
+		then 
+		    kill_script = true	
+	   fi
+	   if[ $kill_script ];
 		then
-			log "Matching record found."
-			
-			current_ip=$(echo $i | jq '.content' | tr -d '"')
-			
-			if [[ "$current_ip" == "$ip" ]];
-			then 
-
-				kill_script = true
-			
-			fi
-
-			if[ $kill_script ];
-			then
-				log "Current ip is set. Script will quit now."
-				kill -s TERM $TOP_PID
-			else
-				rec_id=$(echo $i | jq '.id' | tr -d '"')
-				DeleteRecord "$rec_id"
-			fi	
-		fi
-	done
+		    log "Current ip is set. Script will quit now."
+		    kill -s TERM $TOP_PID
+		else
+		    rec_id=$(echo $i | jq '.id' | tr -d '"')
+		    DeleteRecord "$rec_id"
+	   fi	
+    fi
+    done
 }
 	
 function CreateDNSRecord() {
-
 	log "Creating DNS Record."
-
 	createdns_url="$base_url$dns_zone/$zone_id/records"
 	record_content="[{\"name\":\"$domain\",\"type\":\"$dns_type\",\"content\":\"$ip\",\"ttl\":60,\"prio\":0,\"disabled\":false}]"
-
 	curl -X POST $createdns_url -H "accept: */*" -H "$curl_param $api_key" -H "Content-Type: application/json" -d "$record_content"
 }
 
@@ -142,11 +119,10 @@ function CheckIP() {
 		log "Ip set to $ip" 
 	else
 		log "Adress isn't valid or set. 
-This script will search for the actual ip adress of this machine."
+                     This script will search for the actual ip adress of this machine."
 		RetrieveIpAdress
 	fi
 }
-
 
 ###################################
 ########## START ##################
@@ -155,24 +131,19 @@ This script will search for the actual ip adress of this machine."
 # Get Flags
 while getopts "ha:ef:v" opt; do
         case $opt in
-			# display help
-			h) Help;;
-
-			# ip adress
-			a) ip=$OPTARG;;
-
-			# show error codes
-			e) ErrorCodes exit;;
-
-			# redirect verbose output to file
-			f) redirect_mode=true && redirect_file=$OPTARG;;
-
-			# verbose mode
-			v) verbose_mode=true;;
-
-			# invalid options
-			\?) echo "Error: Invalid options"
-				exit 1;;
+	# display help
+            h) Help;;
+		# ip adress
+            a) ip=$OPTARG;;
+		# show error codes
+	    e) ErrorCodes exit;;
+		# redirect verbose output to file
+	    f) redirect_mode=true && redirect_file=$OPTARG;;
+		# verbose mode
+            v) verbose_mode=true;;
+		# invalid options
+ 	    \?) echo "Error: Invalid options"
+	    exit 1;;
         esac
 done
 
